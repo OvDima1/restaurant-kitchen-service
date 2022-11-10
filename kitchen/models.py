0099ae3f4ff2976c32ff6b7bdc6baabd96af7970
Dima Ovcharenko <kukuku381@gmail.com>
2022-11-10 23:30:09 +0200

    Added models

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(null=True, default=0)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("kitchen:cook-detail", kwargs={"pk": self.pk})
+
+
+class Dish(models.Model):
+    model = models.CharField(max_length=255)
+    description = models.TextField(max_length=255, default="Tasty dish!")
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    dishtype = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    def __str__(self):
+        return self.model
